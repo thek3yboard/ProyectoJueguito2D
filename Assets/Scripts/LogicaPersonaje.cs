a18c7f3fe6d515bd0640769abf1b3994a0bd3fee
thek3yboard <juanileiva17@gmail.com>
2019-10-29 20:23:03 -0300

    Se le hizo de todo. Ahora el player y zombie son distintos. Las animaciones de caminar y el script de seguir al jugador funcionan bien

---
+++ Assets/Scripts/LogicaPersonaje.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPersonaje : MonoBehaviour
+{
+    public float velocidad = 2f;
+    public Vector2 direccion;
+    private Animator animator;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        Movimiento();
+        PresionarBotones();
+
+    	if(Input.GetKey(KeyCode.LeftShift))
+        {
+            velocidad=5f;
+        }
+        else
+        {
+            velocidad=2f;
+        }
+    }
+
+    public void Movimiento()
+    {
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+
+        if(direccion.x != 0 || direccion.y !=0)
+        {
+            AnimarMovimiento(direccion);
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+        
+    }
+    public void PresionarBotones()
+    {
+        direccion = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direccion += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direccion += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direccion += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direccion += Vector2.right;
+        }
+    }
+
+    public void AnimarMovimiento(Vector2 direccion)
+    {
+        animator.SetLayerWeight(1, 1);
+        animator.SetFloat("x", direccion.x);
+        animator.SetFloat("y", direccion.y);
+    }
+}
---
+++ Assets/Scripts/MovimientoWachin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoWachin : MonoBehaviour
+{
+    public float velocidadMovimiento = 2f;
+    public Rigidbody2D rb;
+
+
+    Animator wachinAnimator;
+    
+    bool voltearWachin = true;
+    SpriteRenderer WachinRender;
+
+    Vector2 movimiento;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        rb = GetComponent<Rigidbody2D>();
+        WachinRender = GetComponent<SpriteRenderer>();
+        wachinAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        movimiento.x = Input.GetAxisRaw("Horizontal");
+        movimiento.y = Input.GetAxisRaw("Vertical");
+        
+
+        float mover = Input.GetAxis("Horizontal");
+
+        if (mover > 0 && !voltearWachin){
+                Voltear();
+            }else if(mover < 0 && voltearWachin){
+                Voltear();
+        }
+
+        //sprint
+       
+        if(Input.GetKey(KeyCode.LeftShift))
+            velocidadMovimiento=5f;
+        else
+            velocidadMovimiento=2f;
+
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movimiento * velocidadMovimiento * Time.fixedDeltaTime);
+
+        wachinAnimator.SetFloat("VelMovimiento", Mathf.Abs(movimiento.x));
+    }
+
+    void Voltear(){
+        voltearWachin = !voltearWachin;
+        WachinRender.flipX = !WachinRender.flipX;
+    }
+
+
+}
