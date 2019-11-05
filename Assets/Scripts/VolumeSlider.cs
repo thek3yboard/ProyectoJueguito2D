10c16b9f96594007b25a1a49a50f7a2beafcec45
thek3yboard <juanileiva17@gmail.com>
2019-11-04 23:38:02 -0300

    Se le agregó una barra de volúmen y otro tipo de zombie. Ahora el soundtrack es de 5 canciones.

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+	public Slider volume;
+	public AudioSource music;
+
+	public float volumeOverTime;
+	public float previousValue = 0;
+
+	void Start()
+	{
+		volume.value = 0.5f;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        music.volume = volume.value;
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+        	volume.value -= volumeOverTime * Time.deltaTime;
+        	music.volume = volume.value;
+        	//	music.volume -= volumeOverTime * Time.deltaTime;
+        	//	music.volume = volume.value;
+        }else if(Input.GetKey(KeyCode.E)){
+        	volume.value += volumeOverTime * Time.deltaTime;
+        	music.volume = volume.value;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+        	if(music.volume > 0){
+        		previousValue = volume.value;
+        		music.mute = !music.mute;
+            	volume.value = 0;
+        	}else{
+        		volume.value = previousValue;
+        		music.mute = !music.mute;
+        	}
+        }       
+    }
+}
