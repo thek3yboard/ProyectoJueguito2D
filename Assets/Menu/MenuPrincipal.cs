119d2e023d02acea0de37684a0426d0f1f2c5355
thek3yboard <juanileiva17@gmail.com>
2019-11-11 02:36:01 -0300

    Se agregó botón de Instrucciones, se solucionó error que hacía que se bugeara el menú y se arreglaron algunos colliders del mapa

--- Assets/Menu/MenuPrincipal.cs
+++ Assets/Menu/MenuPrincipal.cs
@@ -5,21 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
-    // Update is called once per frame
-    void Update()
+	public GameObject instructionsImage;
+    public GameObject botonRegresar;
+	
+	//public Button botonRegresar;
+	
+    // Start is called before the first frame update
+    /*
+    void start()
     {
-        
+    	//botonRegresar = GetComponent<Button>();
+    	//botonRegresar.interactable = false;
+
     }
+    */
 
     public void EmpezarJuego()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MostrarInstrucciones()
+    {
+    	instructionsImage.SetActive(true);
+
+        botonRegresar.SetActive(true);
+
+    	//botonRegresar.interactable = true;
+    	//botonRegresar.interactable = true;
+    	/*
+    	if(botonRegresar.onClick.AddListener(TaskOnClick))
+    	{
+    		Debug.Log("tu viejaA EN TANGA");
+    	}
+    	*/
+    	//mybutton.image.overrideSprite = blockA;
+    }
+
+    public void VolvelAlMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void CerrarJuego()
--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -9,6 +9,8 @@ public class SongManager : MonoBehaviour
     private AudioSource audioSource;
  	public AudioClip[] shoot;
  	private AudioClip shootClip;
+ 	public string[] songName = {"Hotline","Miami 2","Perturbator","A New Morning","Electric Dreams"};
+ 	public string actualSongName;
  	public bool pausedSong;
  	public int index;
 
@@ -16,38 +18,39 @@ public class SongManager : MonoBehaviour
     public Text mostrarCancionText;
 
  	//public class MusicPlayer : MonoBehaviour {
- 	public GameObject VolumeCanvas;
+ 	public GameObject KeepCanvas;
  	
      static SongManager instance = null;
  	
-     void Awake()
-     {
-     	VolumeCanvas.transform.parent = null;
-         if (instance != null)
-         {
-             Destroy(gameObject);
-             Destroy(VolumeCanvas);
-         }
-         else
-         {
-             instance = this;
-             GameObject.DontDestroyOnLoad(gameObject);
-             DontDestroyOnLoad(VolumeCanvas);
-         }
-     }
+     
  
  	
 	 	void Start()
 	 	{
+	 		//Scene currentScene = SceneManager.GetActiveScene ();
+
+	 		//string sceneName = currentScene.name;
+	     	
+	 		if(SceneManager.GetActiveScene().name == "Menu")
+	 		{
+	 			KeepCanvas.SetActive(false);
+	 			mostrarCancionPanel.SetActive(false);
+	 		}
+	 		//	return;
+
+
+
 	     	audioSource = gameObject.GetComponent<AudioSource>();
 	     	//shootClip = shoot[0];
 			//cambiarCancion(shootClip);
 			//pausedSong = false;
 
 			shoot = Resources.LoadAll<AudioClip>("Soundtrack");
+			//songName = new string[4]{"Hotline","Miami 2","Perturbator","A New Morning","Electric Dreams"};
 			shootClip = shoot[0];
+			actualSongName = songName[0];
 			index = 0;
-			cambiarCancion(shootClip);
+			cambiarCancion(shootClip, actualSongName);
 			pausedSong = false;
 			/*
 	        foreach(AudioClip clip in shoot)
@@ -57,8 +60,34 @@ public class SongManager : MonoBehaviour
 	        */
 	 	}
 
+
+		void Awake()
+	    {
+	    		KeepCanvas.transform.parent = null;
+			    //SongNameCanvas.transform.parent = null;
+			    if (instance != null)
+			    {
+			        Destroy(gameObject);
+			        Destroy(KeepCanvas);
+			        //Destroy(SongNameCanvas);
+			    }
+			    else
+			    {
+			        instance = this;
+			        GameObject.DontDestroyOnLoad(gameObject);
+			        DontDestroyOnLoad(KeepCanvas);
+			        //DontDestroyOnLoad(SongNameCanvas);
+		   		}
+    	}
+
+
 		void Update()
 		{
+			if(SceneManager.GetActiveScene().name == "Nivel1")
+	 		{
+	 			KeepCanvas.SetActive(true);
+	 			mostrarCancionPanel.SetActive(true);
+	 		}
 			//yield return new WaitForSeconds(shootClip.length);
 			//int index = Random.Range(0, shoot.Length);
 		    //shootClip = shoot[index];
@@ -104,12 +133,12 @@ public class SongManager : MonoBehaviour
 	            audioSource.mute = !audioSource.mute;
 		}
 
-		void cambiarCancion(AudioClip shootClip)
+		void cambiarCancion(AudioClip shootClip, string s)
 		{
 			audioSource.clip = shootClip;
 		    audioSource.Play();
-		    //mostrarCancionPanel.SetActive(true);
-        	//mostrarCancionText.text = "Playing: " + shootClip.ToString;
+		    mostrarCancionPanel.SetActive(true);
+        	mostrarCancionText.text = "LISTENING  TO:    " + actualSongName;
 
         	//m_Text.text = "GameObject Name : " + gameObject.ToString();
 		}
@@ -142,14 +171,16 @@ public class SongManager : MonoBehaviour
 		    	index = 0;
 		    	shootClip = shoot[0];
 		    	Debug.Log("de la ultima paso a la primera");
-		        cambiarCancion(shootClip);
+		    	actualSongName = songName[index];
+		        cambiarCancion(shootClip, actualSongName);
 		    }else{
 		    	shootClip = shoot[index+1];
 		    	
 		    	Debug.Log("de la " + index + " paso a la ");
 		    	index+=1;
 		    	Debug.Log(" " + index);
-		    	cambiarCancion(shootClip);
+		    	actualSongName = songName[index];
+		        cambiarCancion(shootClip, actualSongName);
 		    }
 
 		    Debug.Log("salimo de la funcion muchacho " + index);
